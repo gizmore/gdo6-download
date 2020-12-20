68f88e3645ab518124fe37f65d613ff776ecf76a
gizmore <gizmore@wechall.net>
2020-12-20 09:29:49 +0100

    6.10-r9387_backup_and_fixes

--- Method/Admin.php
+++ Method/Admin.php
@@ -5,20 +5,18 @@ use GDO\Core\MethodAdmin;
 use GDO\Download\GDO_Download;
 use GDO\Table\MethodQueryTable;
 
+/**
+ * Staff overview of downloads.
+ * @author gizmore
+ * @version 6.10
+ * @since 6.04
+ */
 final class Admin extends MethodQueryTable
 {
 	use MethodAdmin;
 
 	public function getPermission() { return 'staff'; }
 	
-	public function execute()
-	{
-		$response = parent::execute();
-		return $this->renderNavBar('Download')->add($response);
-	}
-	
-	public function getQuery()
-	{
-		return GDO_Download::table()->select();
-	}
+    public function gdoTable() { return GDO_Download::table(); }
+
 }
